d189c683705769c8c270d7cc52c6d1e6357f5b75
Lateefah <teefahlateef@outlook.com>
2023-12-30 15:11:25 +0200

    seeder

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import bodyParser from "body-parser";
 import router from "./routes/index.js";
+import db from "./config/db.config.js";
+import Roles from "./models/role.model.js";
+import WorkDone from "./models/workdone.model.js";
 
 dotenv.config();
 
@@ -35,6 +38,51 @@ app.use(express.json());
 app.use("/api/v1", router);
 
 const port = 8080;
+
+db.sync({ force: false }).then(async () => {
+  // Check if the "roles" table is empty
+  const roleCount = await Roles.count();
+  const workCount = await WorkDone.count();
+
+  if (roleCount === 0) {
+    // Insert default roles
+    await Roles.bulkCreate([
+      { id: "1", name: "student" },
+      { id: "2", name: "dept_sup" },
+      { id: "3", name: "comp_sup" },
+      { id: "4", name: "admin" },
+    ]);
+
+    console.log('Default roles inserted into the "roles" table.');
+  } else {
+    console.log('no need for insert "roles" table.');
+  }
+
+  if (workCount === 0) {
+    // Insert default roles
+    await WorkDone.bulkCreate([
+      { workid: "1", work: "Developing Software" },
+      { workid: "2", work: "Operating system installation and maintenance" },
+      {
+        workid: "3",
+        work: "Working as part of a team in a large software project",
+      },
+      { workid: "4", work: "Hardware fault diagnosis and repairs" },
+      { workid: "5", work: "Designing WEB pages" },
+      {
+        workid: "6",
+        work: "Developing a WEB application using ASP, .NET, PHP etc",
+      },
+      { workid: "7", work: "Designing/working with databases" },
+      { workid: "8", work: "Learning to use complex company software" },
+      { workid: "9", work: "Network installation and maintenance" },
+    ]);
+
+    console.log('Default work inserted into the "workdone" table.');
+  } else {
+    console.log('no need for insert "workdone" table.');
+  }
+});
 
 process.on("uncaughtException", (err) => {
   console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
